77df165fa512cc2ea303b55db08a6592c638528e
Allison McEntire <allisonmcentire@gmail.com>
2023-06-11 10:52:21 -0400

    first commit

---
+++ robots/robotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  robotsApp.swift
+//  robots
+//
+//  Created by Allison McEntire on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct robotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
